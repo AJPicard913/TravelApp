b2d145a56ff47f309da3b58a29f41fcd57d4ad18
ajpicard913 <Contact@saucedesigns.io>
2020-09-23 13:10:45 -0400

    Initial Commit

---
+++ Shared/DribbbleApp.swift
@@ -0,0 +1,17 @@
+//
+//  DribbbleApp.swift
+//  Shared
+//
+//  Created by AJ Picard on 9/23/20.
+//
+
+import SwiftUI
+
+@main
+struct DribbbleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
